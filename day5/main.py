61c513d41671b5b9d0f2c7f139f29942061e7a1d
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2023-12-06 15:57:28 -0500

    feat: remove stupid loops

--- day5/main.py
+++ day5/main.py
@@ -1,54 +1,45 @@
 #!/usr/bin/env python
 
 
-# TODO implement method to test if seed number corresponds to
-# move input into desired dataset
-# soil number inside of maps
-# create generator for source/destinations
-# output from each map corresponds to the next map
-# return lowest location number
 class Almanac:
     def __init__(self, order, maps) -> None:
         self.order = order
         self.maps = maps
-        self.seeds = self.maps['seeds']
+        self.seeds = self.maps["seeds"]
         self.location = ...
+        self.lowest_location = ""
 
     def seed_factors(self):
         self.seeds_2 = []
-        seed_combos = [(self.seeds[0], self.seeds[1]), (self.seeds[2], self.seeds[3])]
-        for seed in seed_combos:
-            seeds = range(int(seed[0]), (int(seed[0])+int(seed[1])))
 
-            self.seeds_2.append([*seeds])
+        factors = self.seeds[1::2]
+        seeds = self.seeds[::2]
 
-        self.seeds = [] 
-
-        for l in self.seeds_2:
-            self.seeds += l
+        for i in range(len(seeds)):
+            self.seeds_2.append(range(int(seeds[i]), (int(seeds[i]) + int(factors[i]))))
+        
+        # print(*self.seeds_2[0])
+        self.seeds = self.seeds_2
 
         self.loop_seeds()
 
     def loop_seeds(self):
-        
-        lowest_location = ""
         for seed in self.seeds:
-            self.location = int(seed)
-            print(f"SEED: {seed}")
-            self.map()
+            for s in seed:
+                print(s)
+                self.location = s
+                self.map()
 
-            if lowest_location == "":
-                lowest_location = self.location
-            elif self.location < lowest_location:
-                lowest_location = self.location 
+                if self.lowest_location == "":
+                    self.lowest_location = self.location
+                elif self.location < int(self.lowest_location):
+                    self.lowest_location = self.location
 
-        print('LOWEST LOCATION: ', lowest_location)
+        print("LOWEST LOCATION: ", self.lowest_location)
 
     def map(self):
         for map in self.order:
-            # print(map)
             self.location = self.get_next(self.maps[map], self.location)
-            # print(self.location)
 
     def get_next(self, map, source):
         """processes the map"""
@@ -57,10 +48,10 @@ class Almanac:
             dest = int(i[0])
             src = int(i[1])
             _range = int(i[2])
-            
-            dest_range = range(dest, (dest+_range))
-            source_range = range(src, (src+_range))
-            
+
+            dest_range = range(dest, (dest + _range))
+            source_range = range(src, (src + _range))
+
             if source in source_range:
                 source_index = source_range.index(source)
                 dest_location = dest_range[source_index]
@@ -99,6 +90,7 @@ def load_file(file):
 
 if __name__ == "__main__":
     blocks, title = load_file("./input.txt")
+    # blocks, title = load_file("./test_input.txt")
 
     a = Almanac(title, blocks)
     a.seed_factors()
