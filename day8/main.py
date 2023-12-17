0a1f24ffc180925c5627409b5a085a6fa63790a2
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2023-12-16 19:46:39 -0500

    format: update with black

--- day8/main.py
+++ day8/main.py
@@ -4,26 +4,25 @@
 def navigation_counter(direction, data):
     count = 0
     direction_len = len(direction) - 1
-    j = 0 
+    j = 0
     heading = "AAA"
-    while heading != 'ZZZ':
+    while heading != "ZZZ":
         if j == direction_len:
-            i = direction[j] 
-            j = 0 
+            i = direction[j]
+            j = 0
         else:
             i = direction[j]
             j += 1
 
-        if i == 'R':
-            heading = data[heading][1] 
+        if i == "R":
+            heading = data[heading][1]
         else:
             heading = data[heading][0]
         count += 1
         print(heading)
 
-        if heading == 'ZZZ':
+        if heading == "ZZZ":
             return count
-    
 
 
 def load_file(file):
@@ -36,7 +35,14 @@ def load_file(file):
 
     directions = data[0].strip()
 
-    data = {k: v.split(", ") for k,v in [line.strip().replace('(', '').replace(')','').split(" = ") for line in data[1:] if line != '\n']}
+    data = {
+        k: v.split(", ")
+        for k, v in [
+            line.strip().replace("(", "").replace(")", "").split(" = ")
+            for line in data[1:]
+            if line != "\n"
+        ]
+    }
     return directions, data
 
 
